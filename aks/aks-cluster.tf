14f1f9b4dbbfe43689eb0cc811f2b17b5ef9ab51
Alex Baptista <alexcaio5@gmail.com>
2022-09-27 20:09:28 -0300

    Testing dynamic

--- aks/aks-cluster.tf
+++ aks/aks-cluster.tf
@@ -5,15 +5,17 @@ resource "azurerm_kubernetes_cluster" "default" {
   dns_prefix                        = var.aks-settings.dns_prefix
   role_based_access_control_enabled = var.aks-settings.role_based_access_control_enabled
   tags                              = var.tags
+
   dynamic "default_node_pool" {
-    for_each = var.aks-default-node[*]
+    for_each = var.aks-default-node
     content = {
-      name            = default_node_pool.value.name
-      node_count      = default_node_pool.value.node_count
-      os_disk_size_gb = default_node_pool.value.os_disk_size_gb
-      vm_size         = default_node_pool.value.vm_size
+      name            = default_node_pool.value["name"]
+      node_count      = default_node_pool.value["node_count"]
+      os_disk_size_gb = default_node_pool.value["os_disk_size_gb"]
+      vm_size         = default_node_pool.value["vm_size"]
     }
   }
+
 }
 
 resource "azurerm_resource_group" "default" {
