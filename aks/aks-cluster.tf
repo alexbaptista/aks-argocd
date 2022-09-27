9b4568d7ecfd19c3801742b377aa182ec2b453ac
Alex Baptista <alexcaio5@gmail.com>
2022-09-27 19:40:53 -0300

    refactor terraform

--- aks/aks-cluster.tf
+++ aks/aks-cluster.tf
@@ -1,36 +1,15 @@
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "infra-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "k8s"
-  features            = {}
-
-  default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 30
-  }
-
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
-
-  role_based_access_control_enabled = true
-
-  tags = {
-    Application  = "Demo"
-    Architecture = ""
-    Domain       = ""
-    Environment  = "Infra"
-    MaintainedBy = "Terraform"
-  }
+  name                              = var.aks-name
+  location                          = azurerm_resource_group.default.location
+  resource_group_name               = azurerm_resource_group.default.name
+  dns_prefix                        = var.aks-settings.dns_prefix
+  features                          = var.aks-settings.features
+  role_based_access_control_enabled = var.aks-settings.role_based_access_control_enabled
+  default_node_pool                 = var.aks-default-node
+  tags                              = var.tags
 }
 
-
-
-resource "azurerm_resource_group" "example" {
-  name     = "infra-rg"
-  location = "East US"
+resource "azurerm_resource_group" "default" {
+  name     = var.resourcegroup-name
+  location = var.location
 }
--- aks/variables.tf
+++ aks/variables.tf
@@ -1,7 +1,57 @@
-variable "appId" {
-  description = "Azure Kubernetes Service Cluster service principal"
+variable "location" {
+  type        = string
+  default     = "East US"
+  description = "Azure region for resources"
 }
 
-variable "password" {
-  description = "Azure Kubernetes Service Cluster password"
+variable "aks-name" {
+  type        = string
+  default     = "infra-aks"
+  description = "AKS Cluster name"
+}
+
+variable "aks-default-node" {
+  type = list(object({
+    name            = string
+    node_count      = number
+    os_disk_size_gb = number
+    vm_size         = string
+  }))
+  default = [{
+    name            = "default"
+    node_count      = 1
+    os_disk_size_gb = 30
+    vm_size         = "Standard_D2_v2"
+  }]
+  description = "Settings for AKS Node Cluster"
+}
+
+variable "aks-settings" {
+  type = list(object({
+    dns_prefix                        = string
+    role_based_access_control_enabled = bool
+    features                          = map(string)
+  }))
+  default = [{
+    dns_prefix                        = "k8s"
+    features                          = {}
+    role_based_access_control_enabled = true
+  }]
+  description = "Settings for AKS Cluster"
+}
+
+variable "resourcegroup-name" {
+  type        = string
+  default     = "resourcegroup-aks"
+  description = "Resource Group name"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "Repository"   = "https://github.com/alexbaptista/azuredevops-iac-template-pipeline"
+    "Environment"  = "Infra"
+    "MaintainedBy" = "Terraform"
+  }
+  description = "Relation from TAGS to include in resources"
 }
