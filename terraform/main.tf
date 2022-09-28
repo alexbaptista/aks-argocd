267c6e2b408f4bb3c15b77addd30351bc7b37a58
Alex Baptista <alexcaio5@gmail.com>
2022-09-28 15:20:53 -0300

    fix output var

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,10 +19,10 @@ provider "azurerm" {
 }
 
 provider "kubectl" {
-  host                   = element(azurerm_kubernetes_cluster.default.kube_config.*.host, 0)
-  username               = element(azurerm_kubernetes_cluster.default.kube_config.*.username, 0)
-  password               = element(azurerm_kubernetes_cluster.default.kube_config.*.password, 0)
-  client_certificate     = base64decode(element(azurerm_kubernetes_cluster.default.kube_config.*.client_certificate, 0))
-  client_key             = base64decode(element(azurerm_kubernetes_cluster.default.kube_config.*.client_key, 0))
-  cluster_ca_certificate = base64decode(element(azurerm_kubernetes_cluster.default.kube_config.*.cluster_ca_certificate, 0))
+  host                   = element(azurerm_kubernetes_cluster.default.*.host, 0)
+  username               = element(azurerm_kubernetes_cluster.default.*.username, 0)
+  password               = element(azurerm_kubernetes_cluster.default.*.password, 0)
+  client_certificate     = base64decode(element(azurerm_kubernetes_cluster.default.*.client_certificate, 0))
+  client_key             = base64decode(element(azurerm_kubernetes_cluster.default.*.client_key, 0))
+  cluster_ca_certificate = base64decode(element(azurerm_kubernetes_cluster.default.*.cluster_ca_certificate, 0))
 }
