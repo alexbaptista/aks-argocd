97b34d72194a8ab83c8f3335c24f3e684ee6dbbf
Alex Baptista <alexcaio5@gmail.com>
2022-09-28 20:47:20 -0300

    fix secret value

--- terraform/azurerm_key_vault_secret.tf
+++ terraform/azurerm_key_vault_secret.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_secret" "argocd" {
   name            = var.keyvault_argocd_name
-  value           = data.kubernetes_secret.argocd_admin
+  value           = data.kubernetes_secret.argocd_admin.data["password"]
   key_vault_id    = azurerm_key_vault.default.id
   expiration_date = var.keyvault_argocd_settings.expiration_date
   content_type    = var.keyvault_argocd_settings.content_type
