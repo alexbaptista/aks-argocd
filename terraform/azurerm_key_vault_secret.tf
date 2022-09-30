bbdad63c60a7b01c07aeb0db95f7621e97389682
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 10:26:30 -0300

    comment other blocks

--- terraform/azurerm_key_vault_secret.tf
+++ terraform/azurerm_key_vault_secret.tf
@@ -1,8 +1,8 @@
-resource "azurerm_key_vault_secret" "argocd" {
-  name            = var.keyvault_argocd_name
-  value           = data.kubernetes_secret.argocd_admin.data["password"]
-  key_vault_id    = azurerm_key_vault.default.id
-  expiration_date = var.keyvault_argocd_settings.expiration_date
-  content_type    = var.keyvault_argocd_settings.content_type
-  tags            = var.tags
-}
+# resource "azurerm_key_vault_secret" "argocd" {
+#   name            = var.keyvault_argocd_name
+#   value           = data.kubernetes_secret.argocd_admin.data["password"]
+#   key_vault_id    = azurerm_key_vault.default.id
+#   expiration_date = var.keyvault_argocd_settings.expiration_date
+#   content_type    = var.keyvault_argocd_settings.content_type
+#   tags            = var.tags
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -55,8 +55,8 @@ output "cluster_password" {
   sensitive   = true
 }
 
-output "argocd_admin_password" {
-  value       = data.kubernetes_secret.argocd_admin
-  description = "ArgoCD Admin Password"
-  sensitive   = true
-}
+# output "argocd_admin_password" {
+#   value       = data.kubernetes_secret.argocd_admin
+#   description = "ArgoCD Admin Password"
+#   sensitive   = true
+# }
