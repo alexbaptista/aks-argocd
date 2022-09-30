061947c79ee6fb254c3f91a878b625dc5eb19b2c
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 10:15:15 -0300

    testing without count

--- terraform/kubectl_manifest.tf
+++ terraform/kubectl_manifest.tf
@@ -1,6 +1,7 @@
 resource "kubectl_manifest" "argocd_namespace" {
-  count              = length(data.kubectl_file_documents.argocd_namespace.documents)
-  yaml_body          = element(data.kubectl_file_documents.argocd_namespace.documents, count.index)
+  # count              = length(data.kubectl_file_documents.argocd_namespace.documents)
+  # yaml_body          = element(data.kubectl_file_documents.argocd_namespace.documents, count.index)
+  yaml_body          = data.kubectl_file_documents.argocd_namespace.documents
   override_namespace = var.argocd_settings["namespace"]
   depends_on = [
     azurerm_kubernetes_cluster.default,
@@ -8,8 +9,9 @@ resource "kubectl_manifest" "argocd_namespace" {
 }
 
 resource "kubectl_manifest" "argocd" {
-  count              = length(data.kubectl_file_documents.argocd.documents)
-  yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  # count              = length(data.kubectl_file_documents.argocd.documents)
+  # yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  yaml_body          = data.kubectl_file_documents.argocd.documents
   override_namespace = var.argocd_settings["namespace"]
   depends_on = [
     azurerm_kubernetes_cluster.default,
