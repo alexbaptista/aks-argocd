a75431484dcefa34b93c4fa2c6c898a67b986f54
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 09:40:27 -0300

    add depend on to all argo resources

--- terraform/kubectl_file_documents.tf
+++ terraform/kubectl_file_documents.tf
@@ -1,13 +1,13 @@
 data "kubectl_file_documents" "argocd_namespace" {
+  content = file("../argocd/${var.argocd_settings["version"]}/manifests/namespace.yaml")
   depends_on = [
     azurerm_kubernetes_cluster.default,
   ]
-  content = file("../argocd/${var.argocd_settings["version"]}/manifests/namespace.yaml")
 }
 
 data "kubectl_file_documents" "argocd" {
+  content = file("../argocd/${var.argocd_settings["version"]}/manifests/install.yaml")
   depends_on = [
     azurerm_kubernetes_cluster.default,
   ]
-  content = file("../argocd/${var.argocd_settings["version"]}/manifests/install.yaml")
 }
--- terraform/kubectl_manifest.tf
+++ terraform/kubectl_manifest.tf
@@ -2,13 +2,17 @@ resource "kubectl_manifest" "argocd_namespace" {
   count              = length(data.kubectl_file_documents.argocd_namespace.documents)
   yaml_body          = element(data.kubectl_file_documents.argocd_namespace.documents, count.index)
   override_namespace = var.argocd_settings["namespace"]
+  depends_on = [
+    azurerm_kubernetes_cluster.default,
+  ]
 }
 
 resource "kubectl_manifest" "argocd" {
-  depends_on = [
-    kubectl_manifest.argocd_namespace,
-  ]
   count              = length(data.kubectl_file_documents.argocd.documents)
   yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
   override_namespace = var.argocd_settings["namespace"]
+  depends_on = [
+    azurerm_kubernetes_cluster.default,
+    kubectl_manifest.argocd_namespace,
+  ]
 }
