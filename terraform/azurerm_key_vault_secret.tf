360e60915849a58a1db86fbbbbc2c6a0c3adc795
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 13:07:32 -0300

    fix output

--- terraform/azurerm_key_vault_secret.tf
+++ terraform/azurerm_key_vault_secret.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_secret" "argocd" {
   name            = var.keyvault_argocd_name
   value           = data.kubernetes_secret.argocd_admin.data["password"]
-  key_vault_id    = data.terraform_remote_state.aks.key_vault_id
+  key_vault_id    = data.terraform_remote_state.aks.outputs.key_vault_id
   expiration_date = var.keyvault_argocd_settings.expiration_date
   content_type    = var.keyvault_argocd_settings.content_type
   tags            = var.tags
