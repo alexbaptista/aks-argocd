643956eaefcc073f31a7318bd51e782e4e1d979a
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 14:18:39 -0300

    fix rg

--- terraform/terraform_remote_state.tf
+++ terraform/terraform_remote_state.tf
@@ -4,6 +4,7 @@ data "terraform_remote_state" "aks" {
   config = {
     storage_account_name = "terraformalxbap"
     container_name       = "aks"
+    resource_group_name  = "terraform"
     key                  = "terraform.tfstate"
   }
 }
