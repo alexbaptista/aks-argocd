142e01fa31b33e2998f53432ced9c4a42b358a5c
Alex Baptista <alexcaio5@gmail.com>
2022-09-30 10:17:41 -0300

    add depends on

--- terraform/kubectl_file_documents.tf
+++ terraform/kubectl_file_documents.tf
@@ -1,7 +1,13 @@
 data "kubectl_file_documents" "argocd_namespace" {
   content = file("../argocd/${var.argocd_settings["version"]}/manifests/namespace.yaml")
+  depends_on = [
+    azurerm_kubernetes_cluster.default,
+  ]
 }
 
 data "kubectl_file_documents" "argocd" {
   content = file("../argocd/${var.argocd_settings["version"]}/manifests/install.yaml")
+  depends_on = [
+    azurerm_kubernetes_cluster.default,
+  ]
 }
